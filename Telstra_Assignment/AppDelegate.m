d440a6bcb7efe14a179bdf47d0029028ef213c93
manzar_hussain <manzar_hussain@infosys.com>
2018-03-27 11:07:10 +0530

    1.) View ,controller and data models . 2.)Reachability

--- Telstra_Assignment/AppDelegate.m
+++ Telstra_Assignment/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "THomeViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,7 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    THomeViewController* tHomeViewController = [[THomeViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:tHomeViewController];
+    [self.window setRootViewController:navigationController];
+    [self.window makeKeyAndVisible];
     return YES;
+ 
 }
 
 
